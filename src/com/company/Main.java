2c7137c62b88e086f749d7e0a50f38d7afa47371
Андрей <andry.ru12@yandex.ru>
2019-01-15 12:22:40 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+class Test{
+        public static void main(String args[]){
+            String str = "Привет";
+            String result = str.toUpperCase();
+            System.out.println("Result: " + result);
+    }
+}
